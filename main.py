b4e499b09c564927d3f3471588c178efba67491b
JU4N98 <juanpablocabana2@gmail.com>
2023-06-18 22:22:26 -0300

    Adds image normalization code and sample image.
    Signed-off-by: JU4N98 <juanpablocabana2@gmail.com>

---
+++ main.py
@@ -0,0 +1,124 @@
+import os 
+import json
+import pytesseract
+from PIL import Image
+
+MIN_T = 0;
+MAX_T = 200;
+
+def get_distance(pixel_1:tuple, pixel_2:tuple) -> int:
+    """
+        Returns the Manhattan distance between two rgb values.
+    """
+    return abs(pixel_1[0] - pixel_2[0]) + abs(pixel_1[1] - pixel_2[1]) + abs(pixel_1[2] - pixel_2[2])
+
+def get_rgb_mask(rgb: tuple) -> str:
+    """
+        Returns the rgb mask of a rgb tuple.
+    """
+    return str(rgb[0]*(256**2) + rgb[1] * 256 + rgb[2])
+
+def get_mapping() -> dict:
+    """
+        Returns a dictionary that maps every rgb value to a
+        greyscale value [0,255].
+    """
+
+    # try to load mapping from file
+    try:
+        with open("rgb_to_grey.json") as json_file:
+            return json.load(json_file)
+    except:
+        print("rgb_to_grey.json not found, calculating it ...")
+
+    sample = Image.open("./dataset/sample.jpg")
+    image = sample.load()
+
+    # get right-side scale
+    t_scale = []
+    for i in range(49,271): t_scale.append(image[237,i])
+    t_scale = list(reversed(t_scale))
+
+    # assign value between 0 and 255 to every pixel on right-side scale
+    grey_scale = []
+    for i in range(0,222):
+        grey_scale.append(i*255//221)
+    
+    # map every combination in rgb scale to a greyscale value
+    rgb_to_grey = {}
+    for r in range(0,256):
+        for g in range(0,256):
+            for b in range(0,256):
+                idx = -1
+                distance = 255*3 + 1
+                for i in range(0,222):
+                    if(get_distance(t_scale[i],(r,g,b)) < distance):
+                        idx = i
+                        distance = get_distance(t_scale[i],(r,g,b))
+                rgb_to_grey[get_rgb_mask((r,g,b))] = grey_scale[idx];
+
+    with open("rgb_to_grey.json", "w") as json_file:
+        json.dump(rgb_to_grey, json_file)
+    
+    return rgb_to_grey
+
+def get_temperatures(image) -> list:
+    """
+        Returns minimum and maximum temperature from image.
+    """
+
+    cropped_image = []
+    for r in range(290,305):
+        for c in range(0,200):
+            if get_distance(image[c,r],(255,255,255))<250:
+                cropped_image.append((0,0,0))
+            else:
+                cropped_image.append((255,255,255))
+    
+    new_image = Image.new("RGB", (200,15))
+    new_image.putdata(cropped_image)
+
+    recognized = pytesseract.image_to_string(new_image, lang='eng', config='--psm 7')
+    temperatures = []
+    i : int = 0
+    while i < len(recognized):
+        cur = ""
+        while i<len(recognized) and (recognized[i].isdigit() or recognized[i]=='.'):
+            cur += recognized[i]
+            i += 1
+        if cur != "": 
+            temperatures.append(float(cur))
+            i -= 1
+        i += 1
+
+    return sorted(temperatures)
+    
+
+
+def to_greyscale(file_name:str, rgb_to_grey:dict):
+    """
+        Generates greyscale image in absolute scale from rgb one.
+    """
+    
+    file = Image.open("./dataset/" + file_name)
+    image = file.load()
+
+    temperatures = get_temperatures(image)
+    if len(temperatures) != 2:
+        return
+
+    min_t = temperatures[0]
+    max_t = temperatures[1]
+
+    greyscale_image = []
+    for r in range (33,292):
+        for c in range (0,234):
+            cur_t = rgb_to_grey[get_rgb_mask(image[c,r])]*int(max_t-min_t) // 255 + int(min_t)
+            greyscale_image.append(cur_t * 255 // MAX_T)
+    
+    new_image = Image.new("L",(234,259))
+    new_image.putdata(greyscale_image)
+    new_image.save("./normalized-dataset/"+file_name)
+
+# rgb_to_grey = get_mapping()
+# to_greyscale("sample.jpg", rgb_to_grey)
