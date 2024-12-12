7486415cb991e6e9a31e5f1d48eb683155cb4613
JU4N98 <juanpablocabana2@gmail.com>
2024-12-12 12:00:57 -0300

    Agrega ultimos cambios.

--- prototype/front/src/Report/CreateReport.js
+++ prototype/front/src/Report/CreateReport.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import {postReport} from "../ApiManager/ApiManager"
 import { useNavigate  } from "react-router-dom";
 import { LoadingPage } from "../LoadingPage/LoadingPage";
+import "../App.css";
 
 const ReportForm = () => {
     const [name, setName] = useState('');
@@ -10,10 +11,6 @@ const ReportForm = () => {
     const navigate = useNavigate();
     const [loading, setLoading] = useState(false);
 
-    const handleView = (reportId) => {
-      navigate(`/listReport/${reportId}`);
-    };
-  
     const handleImageUpload = (event) => {
         const files = Array.from(event.target.files);
         const readers = files.map((file) => {
@@ -47,65 +44,68 @@ const ReportForm = () => {
     return (
       <div>
         {loading ? <LoadingPage/> : 
-          <form 
-            onSubmit={async ()=> handleSubmit()}
-            style = {{
-                textAlign:"left",
-                width:"30%",
-                marginLeft:"35%",
-            }}
-          >
+          <div style={{
+            width:"30%",
+            marginLeft:"35%",
+            textAlign:"center",
+          }}>
             <h2 style={{
-                marginTop:"40px",
-                marginBottom:"40px",
-                textAlign:"center"
-            }}
-            >Crear Reporte</h2>
-            <div className="form-group">
-                <input 
-                    type="text" 
-                    value={name} 
-                    onChange={(e) => setName(e.target.value)} 
-                    placeholder="Nombre" 
-                    required 
-                    style={{
-                        width:"100%",
-                        marginBottom:"10px"
-                    }}
-                />
-            </div>
-            <div className="form-group">
-                <textarea 
-                    value={description} 
-                    onChange={(e) => setDescription(e.target.value)} 
-                    placeholder="Descripcion"
-                    style={{
-                        width:"100%",
-                        height:"135px"
-                    }}
-                />
-            </div>
-            <div className="form-group">
-                <input 
-                    type="file" 
-                    acceot=".jpeg, .png, .jpg"
-                    multiple 
-                    onChange={handleImageUpload} 
-                    required
-                    style={{
-                        width:"100%",
-                        borderColor:"transparent"
-                    }}
-                />
-            </div>
-            <div className="form-group" style={{
-                textAlign:"right"
-            }}>
-                <button type="submit" style={{
-                    borderColor:"transparent"
-                }}> Crear reporte </button>
-            </div>
-          </form>
+              marginTop: "40px",
+              marginBottom: "40px",
+              textAlign: "center"
+            }}>Crear Reporte</h2>
+            <form 
+              onSubmit={async ()=> handleSubmit()}
+              style = {{
+                  textAlign:"left",
+              }}
+            >
+              <div className="form-group">
+                  <input 
+                      type="text" 
+                      value={name} 
+                      onChange={(e) => setName(e.target.value)} 
+                      placeholder="Nombre" 
+                      required 
+                      style={{
+                          width:"100%",
+                          marginBottom:"10px"
+                      }}
+                  />
+              </div>
+              <div className="form-group">
+                  <textarea 
+                      value={description} 
+                      onChange={(e) => setDescription(e.target.value)} 
+                      placeholder="Descripcion"
+                      style={{
+                          width:"100%",
+                          height:"135px"
+                      }}
+                  />
+              </div>
+              <div className="form-group">
+                  <input 
+                      type="file" 
+                      acceot=".jpeg, .png, .jpg"
+                      multiple 
+                      onChange={handleImageUpload} 
+                      required
+                      style={{
+                          width:"100%",
+                          borderColor:"transparent"
+                      }}
+                  />
+              </div>
+              <div className="form-group" style={{
+                  textAlign:"right"
+              }}>
+                  <button type="submit" style={{
+                      borderColor:"transparent"
+                  }}> Crear reporte </button>
+              </div>
+            </form>
+          </div>
         }
       </div>
     );
